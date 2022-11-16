83d0a21a19ffc54300caeed369b341f460272e9b
Liam Powers <liam.t.powers@gmail.com>
2022-11-15 22:59:13 -0500

    Add project files.

---
+++ Character-Helper/Controllers/CharacterController.cs
@@ -0,0 +1,28 @@
+﻿using Character_Helper.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Character_Helper.Controllers
+{
+    public class CharacterController : Controller
+    {
+
+        private static List<CharacterViewModel> chars = new List<CharacterViewModel>();
+
+        public IActionResult Index()
+        {
+            return View(chars);
+        }
+
+        public IActionResult Create()
+        {
+            var charVM = new CharacterViewModel();
+            return View(charVM);
+        }
+        
+        public IActionResult CreateCharacter(CharacterViewModel charVM)
+        {
+            chars.Add(charVM);
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
---
+++ Character-Helper/Models/CharacterViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Character_Helper.Models
+{
+    public class CharacterViewModel
+    {
+        public string Name { get; set; }
+        public string Player { get; set; }
+    }
+}
